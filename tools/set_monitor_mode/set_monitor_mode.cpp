bd526d8a1d36157332458478628fa3fd28604241
Kev1MSL <kevinmessali06@gmail.com>
2023-01-31 16:39:14 +0100

    Automated monitor mode

---
+++ tools/set_monitor_mode/set_monitor_mode.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Kevin on 31/01/2023.
+//
+
+#include "set_monitor_mode.h"
+
+void set_monitor_mode(const std::string &iface)
+{
+    std::string cmd = "sudo ifconfig " + iface + " down";
+    system(cmd.c_str());
+    cmd = "sudo iwconfig " + iface + " mode monitor";
+    system(cmd.c_str());
+    cmd = "sudo ifconfig " + iface + " up";
+    system(cmd.c_str());
+    cmd = "sudo iwconfig " + iface + " channel 4";
+    system(cmd.c_str());
+}
---
+++ tools/set_monitor_mode/set_monitor_mode.h
@@ -0,0 +1,12 @@
+//
+// Created by Kevin on 31/01/2023.
+//
+#ifndef PLATFORM_SET_MONITOR_MODE_H
+#define PLATFORM_SET_MONITOR_MODE_H
+
+#include <iostream>
+#include <unistd.h>
+
+void set_monitor_mode(const std::string& iface);
+
+#endif //PLATFORM_SET_MONITOR_MODE_H
